7c2a82eafb93455035f1af59ebef9ce7d62473f9
luismangubat <joseluismangubat@gmail.com>
2021-11-25 20:20:07 -0500

    Added Atari functionality

--- src/atari_env/atari.py
+++ src/atari_env/atari.py
@@ -1,27 +1,57 @@
 import numpy as np
+import gym
+from skimage.transform import resize
+from skimage.color import rgb2gray
+import matplotlib.pyplot as plt
+from skimage import io 
 
 
 class Atari(object):
-    def __init__(self, **kwargs):
-
-        # What agent needs
-        self.max_steps = None # Max steps per episode (int)
-        self.action_space_size = None # The number of actions an agent can perform (int)
-
-    def reset(self, **kwargs) -> np.array:
+    def __init__(self, game: str, max_steps: int, resized_width: int, resized_height: int):
+        self.env = gym.make(game)
+        self.env.reset()
+        self.max_steps = max_steps # Max steps per episode (int)
+        self.action_space_size = self.env.action_space # The number of actions an agent can perform (int)
+        self.resized_width = resized_width
+        self.resized_height = resized_height
+        #self.q_table = np.zeros([self.env.observation_space.n, self.env.action_space.n])
+        
+    def reset(self, **kwargs):
         # Should reset the environment to the begining
         # Returns initial state
-        raise NotImplementedError
+        self.env.reset()
 
-    def step(self, action: int) -> tuple:
+    def num_actions_available(self):
+        # Return total number of actions
+        return self.env.action_space.n
+
+    def get_preprocessed_frame(self, observation):
+        #1) Get image grayscale
+        #2) Rescale image
+        image = resize(rgb2gray(observation), (self.resized_width, self.resized_height))
+        return image[26:, :]
+
+    def print_action_meanings(self):
+        print(self.env.get_action_meanings())
+
+
+        # -> tuple
+    def step(self, action: int):
         # take a step in the env and return the next state, reward,
         # and if the game is done as a tuple
-        raise NotImplementedError
-
+        observation, reward, done, lives = self.env.step(action)
+        image = self.get_preprocessed_frame(observation)
+        print(image.shape)
+        plt.imshow(image, cmap="gray")
+        plt.show()
+        return image, reward, done, lives
+    
     def render(self):
         # Render the game state
-        raise NotImplementedError
+        for _ in range(self.max_steps):
+            self.env.render()
+            self.step(2)
 
-    def get_image(self):
-        # return an image of the game state
-        raise NotImplementedError
+game = Atari("Breakout-v0", 10000, 110, 84)
+
+print(game.reset())
