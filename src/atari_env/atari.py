85ecd9fc92d57f7bb496e6d6c6ebaa64425f1b40
luismangubat <joseluismangubat@gmail.com>
2021-11-25 20:22:37 -0500

    updated

--- src/atari_env/atari.py
+++ src/atari_env/atari.py
@@ -1,9 +1,7 @@
 import numpy as np
 import gym
-from skimage.transform import resize
-from skimage.color import rgb2gray
+import cv2 
 import matplotlib.pyplot as plt
-from skimage import io 
 
 
 class Atari(object):
@@ -14,7 +12,6 @@ class Atari(object):
         self.action_space_size = self.env.action_space # The number of actions an agent can perform (int)
         self.resized_width = resized_width
         self.resized_height = resized_height
-        #self.q_table = np.zeros([self.env.observation_space.n, self.env.action_space.n])
         
     def reset(self, **kwargs):
         # Should reset the environment to the begining
@@ -26,20 +23,30 @@ class Atari(object):
         return self.env.action_space.n
 
     def get_preprocessed_frame(self, observation):
-        #1) Get image grayscale
-        #2) Rescale image
-        image = resize(rgb2gray(observation), (self.resized_width, self.resized_height))
+        # Convert image to grayscale
+        # Rescale image
+        image = cv2.cvtColor(observation, cv2.COLOR_BGR2GRAY)
+        image = cv2.resize(image, (self.resized_height, self.resized_width))
         return image[26:, :]
 
     def print_action_meanings(self):
+        # Prints meaings of all possible actions
         print(self.env.get_action_meanings())
 
 
-        # -> tuple
     def step(self, action: int):
         # take a step in the env and return the next state, reward,
         # and if the game is done as a tuple
         observation, reward, done, lives = self.env.step(action)
+        
+        # Step function to set rewards to -1, +1 or 0
+        if reward > 0:
+            reward = 1
+        elif reward < 0:
+            reward = -1
+        else:
+            reward = 0 
+            
         image = self.get_preprocessed_frame(observation)
         print(image.shape)
         plt.imshow(image, cmap="gray")
@@ -51,7 +58,8 @@ class Atari(object):
         for _ in range(self.max_steps):
             self.env.render()
             self.step(2)
+    
+
 
 game = Atari("Breakout-v0", 10000, 110, 84)
-
-print(game.reset())
+print(game.step(0))
